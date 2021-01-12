88a75b669456ba7b797e33e02e5b1961c6041d40
China Lao <china.appsell@gmail.com>
2021-01-12 20:01:24 +0800

    Added primary and Secondary colors based on Figma

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,18 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
+const vuetify = new Vuetify({
+    theme: {
+      themes: {
+        light: {
+          primary: '#00B882',
+          secondary: '#1CC8EE',
+          error: '#b71c1c',
+        },
+      },
+    },
+  })
+
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+export default vuetify
