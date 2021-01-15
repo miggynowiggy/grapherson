bf32c59beb7ad59053de2b12f6a38f7605263409
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-01-15 17:12:01 +0800

    fixed login scrolling

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,34 +1,38 @@
 <template>
-    <v-app class="ma-5 pa-5">
-        <v-container fill-height fluid>
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-avatar tile size = 100px class = "mb-10">
-            <img alt = 'Grapherson' src = '../assets/Grapherson.png'/>
-          </v-avatar>
-          <v-text-field
-            label="Email Address"
-            placeholder="Email"
-            filled
-            rounded
-            outlined
-          ></v-text-field>
-          <v-text-field
-            type = 'password'
-            label="Password"
-            placeholder="Password"
-            filled
-            rounded
-            outlined
-          ></v-text-field>
-        </v-row>
-      </v-container>
-    </v-app>
+  <v-container fill-height fluid pa-10>
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-avatar tile size = 100px class = "mb-10">
+        <img alt='Grapherson' src='../assets/Grapherson.png'/>
+      </v-avatar>
+      <v-text-field
+        label="Email Address"
+        type="email"
+        filled
+        rounded
+        outlined
+      ></v-text-field>
+      <v-text-field
+        label="Password"
+        filled
+        rounded
+        outlined
+        :append-icon="showPassword ? 'eye' : 'eye-off'"
+        :type="showPassword ? 'text' : 'password'"
+        @click:append="showPassword = !showPassword"
+      ></v-text-field>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 
-export default {}
+export default {
+  name: 'Login',
+  data: () => ({
+    showPassword: false
+  })
+}
 </script>
