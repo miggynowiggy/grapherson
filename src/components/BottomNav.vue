58b09c4a2f050e2c3a4b220581815ea06a068784
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-01-20 16:52:56 +0800

    fixed fab button in home page

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -1,60 +1,36 @@
 <template>
-  <v-main>
-   <div>
-    <v-row
-     align="end"
-     no-gutters
-     style="height: 150px;"
-    >
-     <v-col>
-      <v-btn
-       class="secondary"
-       height="70"
-       width="70"
-       fab
-      >
-       <v-icon large>receipt_long</v-icon>
-      </v-btn>
-     </v-col>
-    </v-row>
-   </div>
-   
-   <v-bottom-navigation
-    app
-    fixed
-    grow
-    height="60"
-   >
-    <v-row>
-     <v-col class="ml-5">
-      <v-btn
-       class="secondary--text"
-       width="10"
-       height="58"
-       plain
-      >
-       <v-icon large>receipt_long</v-icon>
-      </v-btn>
-     </v-col>
-     <v-spacer></v-spacer>
-     <v-col class="ml-5">
-      <v-btn
-       class="secondary--text"
-       width="10"
-       height="58"
-       plain
-      >
-       <v-icon large>settings</v-icon>
-      </v-btn>
-     </v-col>
-    </v-row>
-   </v-bottom-navigation>
-  </v-main>
+	<div>
+		<v-bottom-navigation app fixed grow height="60">
+			<v-row align="center" justify="center">
+				<v-col cols="6">
+					<v-btn class="secondary--text" width="10" height="58" plain block>
+						<v-icon large>receipt_long</v-icon>
+					</v-btn>
+				</v-col>
+				<v-col cols="6">
+					<v-btn class="secondary--text" width="10" height="58" plain block>
+						<v-icon large>settings</v-icon>
+					</v-btn>
+				</v-col>
+			</v-row>
+		</v-bottom-navigation>
+		<v-btn class="big-btn" x-large fab elevation="6" color="primary">
+			<v-icon large>camera_alt</v-icon>
+		</v-btn>
+	</div>
 </template>
 
 <script>
-
-export default {
- name: 'BottomNav'
-}
+	export default {
+		name: "BottomNav",
+	};
 </script>
+
+<style>
+	.big-btn {
+		position: fixed;
+		top: 87vh;
+		left: 41vw;
+		z-index: 1000;
+	}
+</style>
