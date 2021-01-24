05f3abe29482f0a51e3099317dc7d703483c5495
Miguel Mendoza <maestropianistro@gmail.com>
2021-01-24 15:26:06 +0800

    set default font to Poppins

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,22 +1,22 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 const vuetify = new Vuetify({
-    icons: {
-      iconfont: 'md' || 'mdi' || 'fa'
-    },
-    theme: {
-      themes: {
-        light: {
-          primary: '#00B882',
-          secondary: '#1CC8EE',
-          error: '#b71c1c',
-        },
-      },
-    },
+	icons: {
+		iconfont: "md" || "mdi" || "fa",
+	},
+	theme: {
+		themes: {
+			light: {
+				primary: "#00B882",
+				secondary: "#1CC8EE",
+				error: "#b71c1c",
+			},
+		},
+	},
 });
 
 Vue.use(Vuetify);
 
-export default vuetify
+export default vuetify;
