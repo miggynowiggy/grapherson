f2a6726012bd0c429fae1fff482045d724ba9102
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-01-24 17:47:15 +0800

    fixed login and signup page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,114 +1,124 @@
 <template>
-  <v-container fill-height fluid pa-10>
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-avatar tile size = 100px class = "mb-10">
-        <img alt='Grapherson' src='../assets/Grapherson.png'/>
-      </v-avatar>
-      <v-form>
-        <v-text-field
-          label="Email Address"
-          type="email"
-          v-model="email"
-          :rules="emailRules"
-          filled
-          rounded
-          outlined
-          color="secondary"
-        ></v-text-field>
-        <v-text-field
-          label="Password"
-          v-model="password"
-          :rules="passwordRules"
-          filled
-          rounded
-          outlined
-          :append-icon="showPassword ? 'visibility' : 'visibility_off'"
-          :type="showPassword ? 'text' : 'password'"
-          @click:append="showPassword = !showPassword"
-          color="secondary"
-        ></v-text-field>
-      </v-form>
-      <v-col
-        align = "center"
-      >
-        <v-btn
-          class = "mb-3 font-weight-black"
-          width="200"
-          large
-          color="secondary"
-          rounded
-          :disabled="buttonDisabled"
-          :loading="buttonDisabled"
-          @click="login"
-        >
-          LOGIN
-        </v-btn>
-        <v-btn
-          class = "mb-3 font-weight-black"
-          width="200"
-          large
-          color="secondary"
-          rounded
-          outlined
-          :disabled="buttonDisabled"
-          :loading="buttonDisabled" 
-          @click="signUp('user')"
-        >
-          SIGN-UP
-        </v-btn>
-        <v-btn
-          class="text-decoration-underline font-weight-black"
-          width="200"
-          large
-          color="secondary"
-          rounded
-          plain 
-          :disabled="buttonDisabled"
-          :loading="buttonDisabled"
-          @click="signUp('guest')"
-        >
-          PROCEED AS GUEST
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+	<v-container class="login-bg" fill-height fluid pa-10>
+		<v-form lazy-validation v-model="isFormValid">
+			<v-row align="center" justify="center" wrap>
+				<v-col cols="12" align="center">
+					<v-avatar tile size="150px" class="mb-10">
+						<img alt="Grapherson" src="../assets/Grapherson.png" />
+					</v-avatar>
+				</v-col>
+				<v-col cols="12">
+					<v-text-field
+						label="Email Address"
+						type="email"
+						v-model="email"
+						:rules="emailRules"
+						filled
+						rounded
+						outlined
+						color="primary"
+					></v-text-field>
+				</v-col>
+				<v-col cols="12" class="mt-n5">
+					<v-text-field
+						label="Password"
+						v-model="password"
+						:rules="passwordRules"
+						filled
+						rounded
+						outlined
+						:append-icon="showPassword ? 'visibility' : 'visibility_off'"
+						:type="showPassword ? 'text' : 'password'"
+						@click:append="showPassword = !showPassword"
+						color="primary"
+					></v-text-field>
+				</v-col>
+				<v-col cols="10">
+					<v-btn
+						class="mb-3 font-weight-black"
+						width="200"
+						large
+						color="primary"
+						depressed
+						rounded
+						block
+						:disabled="buttonDisabled"
+						:loading="buttonDisabled"
+						@click="login"
+					>
+						LOGIN
+					</v-btn>
+				</v-col>
+				<v-col cols="10" class="mt-n4">
+					<v-btn
+						class="mb-3 font-weight-black"
+						width="200"
+						large
+						color="primary"
+						rounded
+						outlined
+						block
+						:disabled="buttonDisabled"
+						:loading="buttonDisabled"
+						@click="signUp('user')"
+					>
+						SIGN-UP
+					</v-btn>
+				</v-col>
+				<v-col cols="10" class="mt-n4">
+					<v-btn
+						class="text-decoration-underline font-weight-black"
+						width="200"
+						large
+						color="primary"
+						rounded
+						plain
+						block
+						:disabled="buttonDisabled"
+						:loading="buttonDisabled"
+						@click="signUp('guest')"
+					>
+						PROCEED AS GUEST
+					</v-btn>
+				</v-col>
+			</v-row>
+		</v-form>
+	</v-container>
 </template>
 
 <script>
+	export default {
+		name: "Login",
+		data: () => ({
+			email: null,
+			password: null,
+			showPassword: false,
+			buttonDisabled: false,
+			choice: null,
+			isFormValid: false,
 
-export default {
-  name: 'Login',
-  data: () => ({
-    email: null,
-    password: null,
-    showPassword: false,
-    buttonDisabled: false,
-    choice: null,
-
-    
-    emailRules: [
-			v => !!v || 'E-mail is required',
-      v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-    ],
-		passwordRules: [
-			v => !!v || 'This field is required',
-      v => (v && v.length >= 6) || 'Minimum of 6 characters',
-    ],
-  }),
-  methods: {
-    async signUp(choice) {
-      this.buttonDisabled = true;
-      this.$router.push({ name: 'Signup', params: {typeOfUser: choice}});
-      this.loginButtonDisabled = false;
-    },
-    async login(){
-      this.buttonDisabled = true;
-      this.$router.push({ name: 'Home' });
-      this.loginButtonDisabled = false;
-    }
-  },
-}
+			emailRules: [
+				(v) => !!v || "E-mail is required",
+				(v) =>
+					/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+					"E-mail must be valid",
+			],
+			passwordRules: [
+				(v) => !!v || "This field is required",
+				(v) => (v && v.length >= 6) || "Minimum of 6 characters",
+			],
+		}),
+		methods: {
+			async signUp(choice) {
+				this.buttonDisabled = true;
+				this.$router.push({ name: "Signup", params: { typeOfUser: choice } });
+				this.loginButtonDisabled = false;
+			},
+			async login() {
+				this.buttonDisabled = true;
+				this.$router.push({ name: "Home" });
+				this.loginButtonDisabled = false;
+			},
+		},
+	};
 </script>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,110 +1,110 @@
 <template>
-  <v-container>
-    <personal-details 
-      v-if="signUpStep === 1" 
-      @nameChange="getNameValue" 
-      @ageChange="getAgeValue" 
-      @genderChange="getGenderValue"
-    />
-    <login-details 
-      v-else 
-      @emailChange="getEmailValue" 
-      @passwordChange="getPasswordValue" 
-      @confirmPasswordChange="getConfirmPasswordValue"
-    />    
-    <v-row
-      justify = "center"
-    >
-      <v-btn
-        class = "mb-5 font-weight-black"
-        width="200"
-        large
-        color="secondary"
-        rounded
-        :disabled="buttonDisabled"
-        :loading="buttonDisabled"
-        @click="proceed"
-      >
-        <span v-if="typeOfUser === 'user' && signUpStep === 1">Next</span>
-        <span v-else>Proceed</span>
-      </v-btn>
-      <v-btn
-        class = "font-weight-black"
-        width="200"
-        large
-        color="secondary"
-        rounded
-        outlined
-        :disabled="buttonDisabled"
-        :loading="buttonDisabled"
-        @click="back"
-      >
-        <span v-if="typeOfUser === 'user' && signUpStep === 2">Back</span>
-        <span v-else>Cancel</span>
-      </v-btn>
-    </v-row>
-  </v-container>
+	<v-container fill-height fluid class="login-bg">
+		<v-row align="center" justify="center">
+			<personal-details
+				v-if="signUpStep === 1"
+				@nameChange="getNameValue"
+				@ageChange="getAgeValue"
+				@genderChange="getGenderValue"
+			/>
+			<login-details
+				v-else
+				@emailChange="getEmailValue"
+				@passwordChange="getPasswordValue"
+				@confirmPasswordChange="getConfirmPasswordValue"
+			/>
+		</v-row>
+		<v-row wrap align="start" justify="center" class="mt-n4">
+			<v-col cols="10">
+				<v-btn
+					class="font-weight-black"
+					width="200"
+					large
+					color="primary"
+					rounded
+					block
+					:disabled="buttonDisabled"
+					:loading="buttonDisabled"
+					@click="proceed"
+				>
+					<span v-if="typeOfUser === 'user' && signUpStep === 1">Next</span>
+					<span v-else>Proceed</span>
+				</v-btn>
+			</v-col>
+			<v-col cols="10" class="mt-2">
+				<v-btn
+					class="font-weight-black"
+					width="200"
+					large
+					color="primary"
+					rounded
+					outlined
+					block
+					:disabled="buttonDisabled"
+					:loading="buttonDisabled"
+					@click="back"
+				>
+					<span v-if="typeOfUser === 'user' && signUpStep === 2">Back</span>
+					<span v-else>Cancel</span>
+				</v-btn>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
-import PersonalDetails from '../components/PersonalDetails.vue'
-import LoginDetails from '../components/LoginDetails.vue'
-export default {
-  name: 'Signup',
-  components: { PersonalDetails, LoginDetails },
-  data: () => ({
-    name: null,
-    age: null,
-    gender: null,
-    email: null,
-    password: null,
-    confirmPassword: null,
-    typeOfUser: null,
-    signUpStep: 1,
-    buttonDisabled: false,
-  }),
-  created() {
-    this.typeOfUser = this.$route.params.typeOfUser;
-  },
-  methods: {
-    async proceed(){
-      if(!this.name || !this.age || !this.gender)
-        return
-      else if(this.typeOfUser === 'user'){
-        if(this.signUpStep === 1)
-            this.signUpStep = 2
-        else if(!this.email || !this.password || !this.confirmPassword)
-          return
-        else
-          this.$router.push({ name: 'Home'});
-      }
-      else
-        this.$router.push({ name: 'Home'});
-    },
-    async back(){
-      if(this.typeOfUser === 'user' && this.signUpStep === 2)
-        this.signUpStep = 1
-      else
-        this.$router.push({ name: 'Login'});
-    },
-    getNameValue(value){
-      this.name = value;
-    },
-    getAgeValue(value){
-      this.age = value;
-    },
-    getGenderValue(value){
-      this.gender = value;
-    },
-    getEmailValue(value){
-      this.email = value;
-    },
-    getPasswordValue(value){
-      this.password = value;
-    },
-    getConfirmPasswordValue(value){
-      this.confirmPassword = value;
-    }
-  },
-}
-</script>PersonalDetails
+	import PersonalDetails from "../components/PersonalDetails.vue";
+	import LoginDetails from "../components/LoginDetails.vue";
+	export default {
+		name: "Signup",
+		components: { PersonalDetails, LoginDetails },
+		data: () => ({
+			name: null,
+			age: null,
+			gender: null,
+			email: null,
+			password: null,
+			confirmPassword: null,
+			typeOfUser: null,
+			signUpStep: 1,
+			buttonDisabled: false,
+		}),
+		created() {
+			this.typeOfUser = this.$route.params.typeOfUser;
+		},
+		methods: {
+			async proceed() {
+				if (!this.name || !this.age || !this.gender) return;
+				else if (this.typeOfUser === "user") {
+					if (this.signUpStep === 1) this.signUpStep = 2;
+					else if (!this.email || !this.password || !this.confirmPassword)
+						return;
+					else this.$router.push({ name: "Home" });
+				} else this.$router.push({ name: "Home" });
+			},
+			async back() {
+				if (this.typeOfUser === "user" && this.signUpStep === 2)
+					this.signUpStep = 1;
+				else this.$router.push({ name: "Login" });
+			},
+			getNameValue(value) {
+				this.name = value;
+			},
+			getAgeValue(value) {
+				this.age = value;
+			},
+			getGenderValue(value) {
+				this.gender = value;
+			},
+			getEmailValue(value) {
+				this.email = value;
+			},
+			getPasswordValue(value) {
+				this.password = value;
+			},
+			getConfirmPasswordValue(value) {
+				this.confirmPassword = value;
+			},
+		},
+	};
+</script>
