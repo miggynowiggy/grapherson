293efc1f98a078b29d269d8d3ff110d85d8f59fb
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-01-25 11:56:13 +0800

    added Settings page

---
+++ src/views/Settings.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-app>
+		<app-bar :pageName="'Settings'" />
+		<v-main>
+			<v-container fluid>
+				<h1 class="primary--text">Wait lang settings to pramis</h1>
+			</v-container>
+		</v-main>
+		<bottom-nav :activeTab="'Settings'" />
+	</v-app>
+</template>
+
+<script>
+	import BottomNav from "@/components/BottomNav.vue";
+	import AppBar from "../components/AppBar.vue";
+	export default {
+		components: { BottomNav, AppBar },
+	};
+</script>
+
+<style></style>
