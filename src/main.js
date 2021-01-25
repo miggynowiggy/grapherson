9ea18efc52083c8e357919f72e22e3c4cc0435b4
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-01-26 01:04:11 +0800

    added lottiefile player

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+import LottiePlayer from "lottie-player-vue";
+
+Vue.config.productionTip = false;
+Vue.use(LottiePlayer);
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	vuetify,
+	render: (h) => h(App),
+}).$mount("#app");
