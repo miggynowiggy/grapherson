ee68cdcb5a16b5d4d18cc22b011c7b52f115ff3b
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-01-25 11:57:06 +0800

    added state for records

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,16 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
 //Import VUEX modules below
-import sample_index from './modules/sample-index';
+import records from "./modules/records";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-    sample_index
-  }
-})
+	state: {},
+	mutations: {},
+	actions: {},
+	modules: {
+		records,
+	},
+});
