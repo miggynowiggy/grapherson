6cab4c1c4d7b981147e8e0733b6bb6874b2bf62e
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-01-29 18:02:15 +0800

    added snackbar component for app

---
+++ src/components/AlertNotif.vue
@@ -0,0 +1,35 @@
+<template>
+	<v-snackbar v-model="notifState" :timeout="3500" :color="type" left top>
+		<span class="body-2">{{ message }}</span>
+	</v-snackbar>
+</template>
+
+<script>
+	export default {
+		name: "AlertNotif",
+		data: () => ({
+			notifState: false,
+			type: "info",
+			message: "",
+		}),
+		methods: {
+			showSuccess(message) {
+				this.message = message;
+				this.type = "success";
+				this.notifState = true;
+			},
+			showWarning(message) {
+				this.message = message;
+				this.type = "warning";
+				this.notifState = true;
+			},
+			showError(message) {
+				this.message = message;
+				this.type = "error";
+				this.notifState = true;
+			},
+		},
+	};
+</script>
+
+<style></style>
