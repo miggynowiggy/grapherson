6f75c325b508b89a9a1eb0df057129f9b8e132b3
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-02-01 14:26:15 +0800

    added firebbase auth onstate change

--- src/main.js
+++ src/main.js
@@ -8,22 +8,42 @@ import { Plugins } from "@capacitor/core";
 const { StatusBar, Device } = Plugins;
 import { defineCustomElements } from "@ionic/pwa-elements/loader";
 import LottiePlayer from "lottie-player-vue";
+import { AUTH } from "./config/firebase";
 
 Vue.config.productionTip = false;
 Vue.use(LottiePlayer);
 
 Device.getInfo().then((info) => {
+	store.commit("plugins/SET_DEVICE_UUID", info.uuid);
 	if (info.platform !== "web") {
 		StatusBar.setOverlaysWebView({ overlay: false });
 		StatusBar.setBackgroundColor({ color: "#00B882" });
 	}
 });
 
-new Vue({
-	router,
-	store,
-	vuetify,
-	render: (h) => h(App),
-}).$mount("#app");
+window.addEventListener("deviceready", () => {
+	store.dispatch("auth/SET_GOOGLE_PROVIDER");
+});
+
+AUTH.onAuthStateChanged(async (user) => {
+	if (user) {
+		await store.dispatch("auth/getUserDetails");
+		new Vue({
+			router,
+			store,
+			vuetify,
+			render: (h) => h(App),
+		}).$mount("#app");
+		router.push({ name: "Home" });
+	} else {
+		new Vue({
+			router,
+			store,
+			vuetify,
+			render: (h) => h(App),
+		}).$mount("#app");
+		router.push({ name: "Login" });
+	}
+});
 
 defineCustomElements(window);
