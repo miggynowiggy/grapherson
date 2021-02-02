22045c89da821c1f1371a89ec66aca340775c542
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-02-02 17:39:37 +0800

    improved currentPage detection on AppBar component

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -4,10 +4,15 @@
 			<v-icon size="31" color="grey darken-1">arrow_back</v-icon>
 		</v-app-bar-nav-icon>
 		<v-toolbar-title class="font-weight-bold title primary--text">{{
-			pageName || ""
+			pageTitle || ""
 		}}</v-toolbar-title>
 		<v-spacer></v-spacer>
-		<v-btn icon medium v-if="pageName !== 'Register' && pageName !== 'Proceed as Guest'" @click="goHome">
+		<v-btn
+			icon
+			medium
+			v-if="currentPage !== 'Signup' && currentPage !== 'GuestSignIn'"
+			@click="goHome"
+		>
 			<v-icon>home</v-icon>
 		</v-btn>
 	</v-app-bar>
@@ -16,10 +21,20 @@
 <script>
 	export default {
 		name: "AppBar",
-		props: ["pageName"],
+		props: ["pageTitle"],
+		created() {
+			this.currentPage = this.$router.currentRoute.name;
+		},
+		data: () => ({
+			currentPage: "",
+		}),
 		methods: {
 			goBack() {
-				this.$router.go(-1);
+				if (this.currentPage === "ViewRecord") {
+					this.$router.push({ name: "Records" });
+				} else {
+					this.$router.go(-1);
+				}
 			},
 			goHome() {
 				this.$router.push({ name: "Home" });
