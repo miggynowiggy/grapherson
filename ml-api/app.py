2b620a61f37f27c2b181d794f3d642490fa414d2
Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-06-18 13:32:59 +0800

    working ml-api deployed to gcp

--- ml-api/app.py
+++ ml-api/app.py
@@ -6,7 +6,7 @@ import download_file
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return 'Welcome to Grapherson ML-API!'
 
@@ -29,4 +29,4 @@ def analyze():
     return jsonify(results), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
